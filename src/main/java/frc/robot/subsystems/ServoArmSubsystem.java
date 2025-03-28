f549ab1fb0063209a64bac39e6c86578cd85064b
Taif Rahman <taif.rahman21@gmail.com>
2025-03-28 19:31:39 -0400

    added servo to shuffleboard

--- src/main/java/frc/robot/subsystems/ServoArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ServoArmSubsystem.java
@@ -8,25 +8,36 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.ServoArmConstants;
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
+import edu.wpi.first.wpilibj.shuffleboard.SuppliedValueWidget;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ServoArmSubsystem extends SubsystemBase {
 
   public static Servo armServo;
+  public boolean on = false;
+  public SuppliedValueWidget<String> onOffWidget;
 
   public ServoArmSubsystem() {
     armServo = new Servo(ServoArmConstants.armServoPort);
+    SmartDashboard.putBoolean("Servo", false);
   }
 
   public void setServoAngle(double angle){
     armServo.setAngle(angle);
+    on = angle < 180;
+    SmartDashboard.putBoolean("Servo", on);
   }
 
   public void setServoAngleZero(){
     armServo.setAngle(0);
+    on = true;
+    SmartDashboard.putBoolean("Servo", on);
   }
 
   @Override
   public void periodic() {
-    
+
   }
 }
